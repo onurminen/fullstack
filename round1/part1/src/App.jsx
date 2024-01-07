157ad923aea73a0535abd7b6441bdcd4da6d0207
Olli Nurminen <olli.nurminen@aalto.fi>
2024-01-07 18:02:48 +0200

    round1 1a

--- round1/part1/src/App.jsx
+++ round1/part1/src/App.jsx
@@ -1,17 +1,36 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
-
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
       <p>
-        {a} plus {b} is {a + b}
+        Hello {props.name}, you are {props.age} years old
       </p>
     </div>
   )
 }
 
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by
+      <a href="https://github.com/onurminen">onurminen</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 19}/>
+      <Hello name={nimi} age={ika} />
+      <Footer />
+    </>
+  )
+}
+
 export default App
+
